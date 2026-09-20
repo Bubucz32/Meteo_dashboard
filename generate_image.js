90b10112a4972b0ebb1554d078b3fd4de4922f34
Bubucz32 <jakub.kacerov4@gmail.com>
2026-09-20 23:21:33 +0200

    Change dimensions for landscape image generation

--- generate_image.js
+++ generate_image.js
@@ -5,8 +5,8 @@ const fs = require('fs');
 // Seznam stránek s definovanou orientací
 const PAGES = [
   // Varianta na šířku (800x480)
-  { html: 'index.html', output: 'index_landscape.png', width: 480, height: 800 },
-  { html: 'index_dark.html', output: 'index_dark_landscape.png', width: 480, height: 800 },
+  { html: 'index.html', output: 'index_landscape.png', width: 800, height: 480 },
+  { html: 'index_dark.html', output: 'index_dark_landscape.png', width: 800, height: 480 },
 
   // Varianta na výšku (480x800)
 //  { html: 'index_portrait.html', output: 'index_portrait.png', width: 480, height: 800 },
